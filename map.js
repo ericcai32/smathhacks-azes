2463ed9193eccde9a815eba484225631b70835dd
Eric Cai <ericcai32@gmail.com>
2024-03-02 21:22:40 -0500

    Updated styling on index.html.

--- map.js
+++ map.js
@@ -9,6 +9,11 @@ function checkLocation() {
     let lat = document.getElementById("lat").value
     let long = document.getElementById("long").value
     let map = document.getElementById("map")
-    let stats = document.getElementById("stats").style.display = "block"
     map.setAttribute("src", "https://www.google.com/maps/embed/v1/view?center=" + lat + "," + long + "&zoom=18&key=AIzaSyCmSzImOzSIVqGLKXi1ppJJWBlc8iYXJm4&maptype=satellite")
+    
+    let stats = document.getElementById("stats")
+    stats.style.display = "block"
+
+    document.getElementById("coords").textContent = lat + ", " + long
+
 }
