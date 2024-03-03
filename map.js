1ee58ff9f936d1abacc1421230dc214e3f4ca56f
Eric Cai <ericcai32@gmail.com>
2024-03-03 03:13:18 -0500

    Prepared all frontend for ML implementation.

--- map.js
+++ map.js
@@ -6,14 +6,13 @@ function changeMap() {
 }
 
 function checkLocation() {
-    let lat = document.getElementById("lat").value
-    let long = document.getElementById("long").value
+    let latlong = document.getElementById("latlong").value
     let map = document.getElementById("map")
-    map.setAttribute("src", "https://www.google.com/maps/embed/v1/view?center=" + lat + "," + long + "&zoom=18&key=AIzaSyCmSzImOzSIVqGLKXi1ppJJWBlc8iYXJm4&maptype=satellite")
+    map.setAttribute("src", "https://www.google.com/maps/embed/v1/view?center=" + latlong + "&zoom=18&key=AIzaSyCmSzImOzSIVqGLKXi1ppJJWBlc8iYXJm4&maptype=satellite")
     
     let stats = document.getElementById("stats")
     stats.style.display = "block"
 
-    document.getElementById("coords").textContent = lat + ", " + long
+    document.getElementById("coords").textContent = latlong
 
 }
